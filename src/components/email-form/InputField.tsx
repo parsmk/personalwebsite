8fc3bb7a718e795abf170d38650e8cdb48d311cf
Parsa MK <p.mohtashami.k@gmail.com>
2026-03-19 23:58:01 -0700

    feat hero landing page

--- src/components/email-form/InputField.tsx
+++ src/components/email-form/InputField.tsx
@@ -17,8 +17,8 @@ export default function InputField({
 }: Props) {
   const _inputClasses = `
     p-2 my-1 w-full rounded-lg outline-1 outline-accent/20
-    focus:outline-accent/75 hover:outline-accent/75
-    transition
+    focus:outline-accent/75 hover:outline-accent/75 hover:shadow-md hover:shadow-primary/25
+    transition-all
     ${inputClasses}
   `;
 
