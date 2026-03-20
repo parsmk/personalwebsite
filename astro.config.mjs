3f79439d815e85d48bf1ae9aca8f871da67da986
Parsa MK <p.mohtashami.k@gmail.com>
2026-03-19 21:50:41 -0700

    init node adapter for api endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
