5da25f3bc86237baf34015ed5600cd9703250802
Parsa MK <p.mohtashami.k@gmail.com>
2026-03-20 01:38:59 -0700

    refactor Hero.tsx

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,20 @@
+export const Hero = () => {
+  return (
+    <div className="flex flex-row items-center justify-center grow gap-2">
+      <img
+        src="hero-bg.jpg"
+        alt="bg-placeholder"
+        className="absolute inset-0 -z-1"
+      />
+      <div className="flex flex-col justify-center items-end rounded-tl-full size-100">
+        <div className="w-[80%]">
+          <h1 className="font-bold text-5xl text-accent">PMK Development</h1>
+          <h2 className="text-2xl text-accent/70 my-2">
+            Software Engineer, Full-Stack Developer
+          </h2>
+        </div>
+      </div>
+      <img src="hero-img.png" className="h-[75%]" />
+    </div>
+  );
+};
