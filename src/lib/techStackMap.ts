1851a3129ed4985eab3907a995dfe5e34e5ec485
Parsa MK <p.mohtashami.k@gmail.com>
2026-05-13 18:40:40 -0700

    code cleanup

--- src/lib/techStackMap.ts
+++ src/lib/techStackMap.ts
@@ -1,88 +1,90 @@
 import type { TechStack } from "../models/TechStack";
 
+const PATH = (fileName: string) => `/icons/${fileName}`;
+
 export const techStackMap: Record<string, TechStack> = {
   ts: {
-    path: "/icons/typescript.svg",
+    path: PATH("typescript.svg"),
     name: "Typescript",
     color: "#3178C6",
   },
   react: {
-    path: "/icons/react.svg",
+    path: PATH("react.svg"),
     name: "ReactJS",
     color: "#61DAFB",
   },
   next: {
-    path: "/icons/nextjs.svg",
+    path: PATH("nextjs.svg"),
     name: "NextJS",
     color: "#000000",
   },
   express: {
-    path: "/icons/express.svg",
+    path: PATH("express.svg"),
     name: "ExpressJS",
     color: "#000000",
   },
   tailwind: {
-    path: "/icons/tailwind.svg",
+    path: PATH("tailwind.svg"),
     name: "TailwindCSS",
     color: "#06B6D4",
   },
   reactQuery: {
-    path: "/icons/reactquery.svg",
+    path: PATH("reactquery.svg"),
     name: "TanStack Query",
     color: "#FF4154",
   },
   postgreSQL: {
-    path: "/icons/postgre.svg",
+    path: PATH("postgre.svg"),
     name: "PostgreSQL",
     color: "#4169E1",
   },
   python: {
-    path: "/icons/python.svg",
+    path: PATH("python.svg"),
     name: "Python",
     color: "#3776AB",
   },
   django: {
-    path: "/icons/django.svg",
+    path: PATH("django.svg"),
     name: "Django REST Framework",
     color: "#092E20",
   },
   shopware: {
-    path: "/icons/shopware.svg",
+    path: PATH("shopware.svg"),
     name: "Shopware",
     color: "#1B9BF0",
   },
   js: {
-    path: "/icons/js.svg",
+    path: PATH("js.svg"),
     name: "Javascript",
     color: "#ff7b00",
   },
   c3: {
-    path: "/icons/c3.svg",
+    path: PATH("c3.svg"),
     name: "C#",
     color: "#662579",
   },
   unity: {
-    path: "/icons/unity.svg",
+    path: PATH("unity.svg"),
     name: "Unity",
     color: "#d6d3d1",
   },
   hlsl: {
-    path: "/icons/hlsl.svg",
+    path: PATH("hlsl.svg"),
     name: "HLSL",
     color: "#0000ff",
   },
   yaml: {
-    path: "/icons/yaml.svg",
+    path: PATH("yaml.svg"),
     name: "YAML",
     color: "#CC2828",
   },
   mongodb: {
-    path: "/icons/mongodb.svg",
+    path: PATH("mongodb.svg"),
     name: "MongoDB",
     color: "#4a8c4f",
   },
   astro: {
-    path: "/icons/astro.svg",
+    path: PATH("astro.svg"),
     name: "Astro",
     color: "#0d0d1a",
   },
