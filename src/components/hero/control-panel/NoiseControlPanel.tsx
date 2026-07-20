3f33f1f963dc80cab6d832f6e8ceb667515a306a
Parsa MK <p.mohtashami.k@gmail.com>
2026-07-20 16:28:57 -0700

    feat scaffolding for intersection observer

--- src/components/hero/control-panel/NoiseControlPanel.tsx
+++ src/components/hero/control-panel/NoiseControlPanel.tsx
@@ -3,9 +3,10 @@ import { NoiseBreak } from "./NoiseBreak";
 import { NoiseConfigProvider } from "./NoiseConfigContext";
 import { NoiseGenSection } from "./NoiseGenSection";
 import { NoiseFractalSection } from "./NoiseFractalSection";
-import { NoiseHeaderSection } from "./NoiseHeaderSection";
+import { NoiseModePills } from "./NoiseModePills";
 
 import type { NoiseConfig } from "../shaders/utils";
+import { Arrow } from "../../svgs/Arrow";
 
 type NoiseControlPanelProps = {
   config: NoiseConfig;
@@ -25,7 +26,32 @@ export const NoiseControlPanel = ({
         bg-accent/75 outline-1 outline-primary/50 rounded-md
       "
     >
-      <NoiseHeaderSection />
+      <div className="flex gap-2 items-center">
+        <div
+          className={`
+            flex items-center justify-center size-10 rotate-270 
+            bg-primary/75 group hover:bg-primary
+            rounded-md border-2 border-secondary/25 hover:border-secondary/50
+            cursor-pointer
+            transition duration-300
+          `}
+        >
+          <Arrow
+            className={`
+              fill-accent/75 size-7.5 
+              group-hover:fill-accent transition duration-300 group-hover:-translate-y-0.5 group-hover:translate-x-0.5
+            `}
+          />
+        </div>
+        <div>
+          <h3 className="text-md text-white">Noise Generator</h3>
+          <p className="text-primary">
+            {`${config.noiseMode.charAt(0).toUpperCase() + config.noiseMode.slice(1)} · ${config.noiseData.size[0]} x ${config.noiseData.size[1]}`}
+          </p>
+        </div>
+      </div>
+
+      <NoiseModePills />
 
       <NoiseBreak />
 
---
+++ src/components/hero/control-panel/NoiseModePills.tsx
@@ -0,0 +1,32 @@
+import { NoiseMode } from "../shaders/utils";
+import { useNoiseConfig } from "./NoiseConfigContext";
+import { NoiseModePill } from "./NoiseModePill";
+
+export const NoiseModePills = () => {
+  const { config, setConfig } = useNoiseConfig();
+
+  return (
+    <div className="gap-2 text-white/50 flex mt-5">
+      <div className="bg-primary/25 gap-1 p-1.5 rounded-lg flex grow overflow-hidden justify-center">
+        <NoiseModePill
+          label="Perlin"
+          active={config.noiseMode === NoiseMode.PERLIN}
+          onClick={() =>
+            config.noiseMode !== NoiseMode.PERLIN
+              ? setConfig({ noiseMode: NoiseMode.PERLIN })
+              : undefined
+          }
+        />
+        <NoiseModePill
+          label="Worley"
+          active={config.noiseMode === NoiseMode.WORLEY}
+          onClick={() =>
+            config.noiseMode !== NoiseMode.WORLEY
+              ? setConfig({ noiseMode: NoiseMode.WORLEY })
+              : undefined
+          }
+        />
+      </div>
+    </div>
+  );
+};
